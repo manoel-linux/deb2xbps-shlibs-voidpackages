eefbc9b160d607912de9f279020017a3da36a136
manoel-linux <135872910+manoel-linux@users.noreply.github.com>
2023-06-09 14:19:59 -0300

    Add files via upload

---
+++ installupdate.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "#################################################################"
+echo "deb2xbps-installer: june 2023 xdeb-based"
+echo "#################################################################"
+echo " ██████  ███████ ██████  ██████  ██   ██ ██████  ██████  ███████ "
+echo " ██   ██ ██      ██   ██      ██  ██ ██  ██   ██ ██   ██ ██      "
+echo " ██   ██ █████   ██████   █████    ███   ██████  ██████  ███████ "
+echo " ██   ██ ██      ██   ██ ██       ██ ██  ██   ██ ██           ██ "
+echo " ██████  ███████ ██████  ███████ ██   ██ ██████  ██      ███████ "
+echo "#################################################################"
+echo "deb2xbps-github: https://github.com/manoel-linux/deb2xbps-shlibs-voidpackages"
+echo "xdeb-github: https://github.com/toluschr/xdeb"
+echo "#################################################################"
+echo "$(date)"
+echo "Linux $(uname -r)"
+echo "#################################################################"
+
+if [[ $EUID -ne 0 ]]; then
+   echo "ERROR! sudo needed" 
+   exit 1
+fi
+
+rm /usr/bin/deb2xbps
+
+cp deb2xbps /usr/bin/
+
+chmod +x /usr/bin/deb2xbps
+
+echo "DONE! Installation/Update completed."
---
+++ uninstall.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "#################################################################"
+echo "deb2xbps-uninstaller: june 2023 xdeb-based"
+echo " ██████  ███████ ██████  ██████  ██   ██ ██████  ██████  ███████ "
+echo " ██   ██ ██      ██   ██      ██  ██ ██  ██   ██ ██   ██ ██      "
+echo " ██   ██ █████   ██████   █████    ███   ██████  ██████  ███████ "
+echo " ██   ██ ██      ██   ██ ██       ██ ██  ██   ██ ██           ██ "
+echo " ██████  ███████ ██████  ███████ ██   ██ ██████  ██      ███████ "
+echo "deb2xbps-github: https://github.com/manoel-linux/deb2xbps-shlibs-voidpackages"
+echo "xdeb-github: https://github.com/toluschr/xdeb"
+echo "$(date)"
+echo "linux $(uname -r)"
+
+if [[ $EUID -ne 0 ]]; then
+   echo "ERROR! sudo needed" 
+   exit 1
+fi
+
+rm /usr/bin/deb2xbps
+
+echo "DONE! Uninstallation completed."
